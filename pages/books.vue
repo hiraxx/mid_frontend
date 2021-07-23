f4142066b701ab5868a0eb2e931bae7abf2267c2
hiraxx <colendrespatricia@gmail.com>
2021-07-24 00:02:42 +0800

    not finished

--- pages/books.vue
+++ pages/books.vue
@@ -1,77 +1,51 @@
 <template>
   <div>
-    <NavBar/>
-    <EditBookModal :book="selectedBook"/>
-    <DeleteBookModal :book="selectedBook" @onDeleteEntry="getAll" />
-    <div class="container mt-2">
-      
-        <h1>
-          <BookEntryModal class="float-right mb-2" @onAddEntry="getAll"/>
-          Books
-          </h1>
-      <table class="table table-striped">
-        <thead class="bg-primary text-light">
-          <th>Title</th>
-          <th>Author</th>
-          <th>Description</th>
-          <th>Book No</th>
-          <th>Date Published</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </thead>
-        <tbody>
-          <tr v-for="book in books" :key="book.id">
-            <td>{{book.title}}</td>
-            <td>{{book.author}}</td>
-            <td>{{book.description}}</td>
-            <td>{{book.book_no}}</td>
-            <td>{{book.date_published}}</td>
-            <td>
-              <b-button @click="onEdit(book)" variant="info" size="sm">Edit</b-button>
-            </td>
-            <td>
-              <b-button @click="onDelete(book)" variant="danger" size="sm">Delete</b-button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+     <NavBar />
+    <div class='container'>
+    <h1> Books </h1>
+    <div class="row">
+      <div class="col-6">
+        <h5>List of Books</h5>
+        <ul class="list group">
+          <li class="list-group-item list-group-item-action" v-for="books in books" :key="books.id">
+            {{books.name}} <span class="float-right">{{books.value}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <BooksView :book="selectedBook" @saved="onSave" />
+      </div>
     </div>
   </div>
+</div>
+
 </template>
-
+      
 <script>
+  export default {
+    data() {
+      return {
+        books: [],
+        selectedBook: {}
 
-export default {
-  data(){
-    return {
-      books: [],
-      selectedBook: {}
-    }
-  },
-  methods: {
-    async getAll(){
-      await this.$axios.get('/api/books')
-      .then((res)=>{
-        if(res.status==200){
-          this.books = res.data
-        }
-      })
+      }
     },
-    onEdit(selectedBook){
-      this.selectedBook = selectedBook
-      this.$bvModal.show('editBookModal')
+    methods: {
+      async getMyBooks() {
+        await this.$axios.get('/Books' )
+        .then((res)=>{
+          if(res.status==200) {
+            this.books = res.data
+          }
+        })
+      },
+      onSave() {
+        this.getMyBooks()
+      }
     },
-    onDelete(selectedBook){
-      this.selectedBook = selectedBook
-      this.$bvModal.show('deleteBookModal')
+    created() {
+      this.getMyBooks()
+      
     }
-  },
-  created(){
-    this.getAll()  
   }
-}
-</script>
-
-<style>
-
-</style>
+      </script>
